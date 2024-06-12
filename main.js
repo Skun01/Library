5aefb652d9a8bc6bf11a3e2abd55bbd32b524d0e
Skun01 <truongg9655@gmail.com>
2024-06-12 23:24:14 +0700

    finish html and css

---
+++ main.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(name, author, pages, isRead){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(book){
+    
+}
